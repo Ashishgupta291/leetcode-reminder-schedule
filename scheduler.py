fe115da403a84afba7e8a88920b3c8a941dfd00e
Ashish Gupta <121349721+Ashishgupta291@users.noreply.github.com>
2025-07-03 13:03:49 +0530

    Add files via upload

--- scheduler.py
+++ scheduler.py
@@ -12,8 +12,10 @@ import time
 import schedule
 load_dotenv()
 # Email config
-SENDER_EMAIL = "ansuashish291@gmail.com"
-SENDER_PASSWORD = "qjeh fdag ylan lgzw"
+# SENDER_EMAIL = "ansuashish291@gmail.com"
+# SENDER_PASSWORD = "qjeh fdag ylan lgzw"
+SENDER_EMAIL = os.getenv("SENDER_EMAIL")
+SENDER_PASSWORD = os.getenv("SENDER_PASSWORD")
 SMTP_SERVER = "smtp.gmail.com"
 SMTP_PORT = 465
 
